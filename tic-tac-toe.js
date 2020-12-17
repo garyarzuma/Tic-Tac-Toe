eb1c25bf7155f12c486b1b9afd7273a38b0c00d6
Gary Arzumanyan <garyarzumanyan@gmail.com>
2020-12-16 20:52:32 -0800

    Created gameBoard module and function to draw basic board in HTML with dummy X and Os

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,27 @@
+//Module with instant function call
+const Gameboard = ( function(){
+    const gameBoardDiv = document.getElementById("gameBoard");
+    const gameSquareDivs = document.querySelectorAll(".gameSquare");
+
+    let gameBoard = [ "X", "O", "X", "X", "O", "X", "X", "O", "X", ];
+
+    const drawBoard = function(){
+        let i=0;
+        gameBoard.forEach(element => gameSquareDivs[i++].textContent = element);
+    }
+    return {drawBoard};
+  })();
+
+Gameboard.drawBoard();
+
+
+
+// factory function for Person 
+const Person = () => {
+    const doSomething = () => console.log("testPerson");
+    return {doSomething};
+}
+
+const player1 = Person();
+
 
